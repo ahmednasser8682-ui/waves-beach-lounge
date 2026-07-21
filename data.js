ae3a5337c9108499a5233be2af02815ff581c35c
ahmednasser8682-ui <ahmed.nasser.8682@gmail.com>
2026-07-21 05:26:57 +0300

    Create menuData object with multilingual categories
    Added menu data with categories and items in Arabic, English, and Russian.

---
+++ data.js
@@ -0,0 +1,181 @@
+const menuData = {
+
+ar:{
+
+categories:[
+
+{
+id:"breakfast",
+icon:"🍳",
+title:"الإفطار",
+
+items:[
+
+{
+name:"أومليت توست",
+desc:"يقدم مع عصير فريش",
+price:"200 EGP"
+},
+
+{
+name:"سالمون توست",
+desc:"سالمون مدخن مع صوص خاص",
+price:"230 EGP"
+}
+
+]
+
+},
+
+{
+id:"burger",
+icon:"🍔",
+title:"برجر",
+
+items:[
+
+{
+name:"رويال برجر",
+desc:"شيدر - باربكيو - تيكساس",
+price:"190 EGP"
+},
+
+{
+name:"بيكون مشروم",
+desc:"بيكون - مشروم - شيدر",
+price:"210 EGP"
+}
+
+]
+
+},
+
+{
+id:"pizza",
+icon:"🍕",
+title:"بيتزا",
+
+items:[
+
+{
+name:"مارجريتا",
+desc:"جبنة موتزاريلا",
+price:"200 EGP"
+},
+
+{
+name:"سوبر سوبريم",
+desc:"ميكس لحوم",
+price:"420 EGP"
+}
+
+]
+
+},
+
+{
+id:"coffee",
+icon:"☕",
+title:"قهوة",
+
+items:[
+
+{
+name:"لاتيه",
+desc:"Espresso + Milk",
+price:"115 EGP"
+},
+
+{
+name:"سبانش لاتيه",
+desc:"Spanish Style",
+price:"130 EGP"
+}
+
+]
+
+}
+
+]
+
+},
+
+//========================
+
+en:{
+
+categories:[
+
+{
+id:"breakfast",
+icon:"🍳",
+title:"Breakfast",
+
+items:[
+
+{
+name:"Omelette Toast",
+desc:"Served with Fresh Juice",
+price:"200 EGP"
+},
+
+{
+name:"Salmon Toast",
+desc:"Smoked Salmon",
+price:"230 EGP"
+}
+
+]
+
+},
+
+{
+id:"burger",
+icon:"🍔",
+title:"Burgers",
+
+items:[
+
+{
+name:"Royal Burger",
+desc:"Cheddar • BBQ • Texas",
+price:"190 EGP"
+}
+
+]
+
+}
+
+]
+
+},
+
+//========================
+
+ru:{
+
+categories:[
+
+{
+id:"breakfast",
+icon:"🍳",
+title:"Завтраки",
+
+items:[
+
+{
+name:"Омлет",
+desc:"Со свежим соком",
+
+price:"200 EGP"
+}
+
+]
+
+}
+
+]
+
+}
+
+}
